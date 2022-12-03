dde36a79e288433f7d8a5045048bc90136f6e596
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-03 18:50:01 +0700

    update env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_DATABASE_CONNECTION:
+      process.env.NEXT_PUBLIC_DATABASE_CONNECTION,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
