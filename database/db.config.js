6056d1fe39bd8ec943766083886fa890d4273643
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-03 18:43:40 +0700

    update env

--- database/db.config.js
+++ database/db.config.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_CONNECTION);
+const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE_CONNECTION);
 
 module.exports = sequelize;
