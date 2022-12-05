ac3e7b9e33bcdf061e984a075047076015a5adf7
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-05 14:48:29 +0700

    test

--- pages/api/testing.js
+++ pages/api/testing.js
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const sequelize = require("../../database/db.config");
+const sequelize = require("../../database/db.config");
 
 export default async function handler(req, res) {
-  // try {
-  //   await sequelize.authenticate();
-  //   console.log("Connection has been established successfully.");
-  // } catch (error) {
-  //   console.error("Unable to connect to the database:", error);
-  // }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
   res.status(200).json({ name: "Hello Tegar" });
 }
