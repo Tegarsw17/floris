f6372789da0bb0057466e8ede3d4ef12d50c7618
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-05 11:56:10 +0700

    check db

--- database/db.config.js
+++ database/db.config.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE_CONNECTION);
+const sequelize = new Sequelize(
+  "postgresql://postgres:kedMYe^bM694XMGFW9kx@db.lprpwvtrtikvmcaizoeg.supabase.co:5432/postgres"
+);
+// const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE_CONNECTION);
 
 module.exports = sequelize;
