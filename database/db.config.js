9a3b21df51074b8698953031b74c06d6e14c472d
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-05 12:42:59 +0700

    cahnge config db

--- database/db.config.js
+++ database/db.config.js
@@ -1,8 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  "postgresql://postgres:kedMYe^bM694XMGFW9kx@db.lprpwvtrtikvmcaizoeg.supabase.co:5432/postgres"
-);
+// const sequelize = new Sequelize(
+//   "postgresql://postgres:kedMYe^bM694XMGFW9kx@db.lprpwvtrtikvmcaizoeg.supabase.co:5432/postgres"
+// );
 // const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE_CONNECTION);
+const sequelize = new Sequelize(
+  "postgres",
+  "postgres",
+  "kedMYe^bM694XMGFW9kx",
+  {
+    host: "db.lprpwvtrtikvmcaizoeg.supabase.co",
+    dialect: "postgres",
+  }
+);
 
 module.exports = sequelize;
