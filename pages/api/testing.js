2045a7c3e9c2e40320f0c940c34fa87870c62d05
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-05 15:24:46 +0700

    cek

--- pages/api/testing.js
+++ pages/api/testing.js
@@ -4,9 +4,9 @@ const sequelize = require("../../database/db.config");
 export default async function handler(req, res) {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    const data = "Connection has been established successfully.";
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    const data = "Unable to connect to the database:" + error;
   }
-  res.status(200).json({ name: "Hello Tegar" });
+  res.status(200).json({ name: "Hello Tegar", data: data });
 }
