c47982a66c1f26f8f7da206017d6b31809ce5890
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-05 16:06:30 +0700

    typo conn DB

--- database/db.config.js
+++ database/db.config.js
@@ -3,12 +3,12 @@ import * as pg from "pg";
 
 // const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DATABASE_CONNECTION);
 const sequelize = new Sequelize(
-  NEXT_PUBLIC_DATABASE,
-  NEXT_PUBLIC_USERNAME,
-  NEXT_PUBLIC_PASSWORD,
+  process.env.NEXT_PUBLIC_DATABASE,
+  process.env.NEXT_PUBLIC_USERNAME,
+  process.env.NEXT_PUBLIC_PASSWORD,
   {
-    host: NEXT_PUBLIC_HOST,
-    dialect: NEXT_PUBLIC_DATABASE,
+    host: process.env.NEXT_PUBLIC_HOST,
+    dialect: process.env.NEXT_PUBLIC_DATABASE,
     dialectModule: pg,
   }
 );
