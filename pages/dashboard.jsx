675d9201587bd44f002b614e3e88886e2b670e65
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-12-13 22:28:17 +0700

    dash update

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -5,6 +5,7 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { useRouter } from "next/router";
 import { setLogout, verifyToken } from "../middleware/utils";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Dashboard = () => {
   const router = useRouter();
@@ -27,15 +28,22 @@ const Dashboard = () => {
 
   return (
     <div className="flex h-screen">
+      {/* Sidebar Content */}
       <div className="w-80 bg-[#49D299]">
-        <div className="h-5/6 m-4">
-          <h2>
-            <AiOutlineDashboard className="inline" />
+        <div className="h-5/6 m-4 p2">
+          <h2 className="text-[#f7f7f7]">
+            <AiOutlineDashboard className="inline pb-1" />
             Floris
           </h2>
         </div>
       </div>
-      <div className="w-full bg-[#f7f7f7]">ini main content</div>
+      {/* Main Content */}
+      <div className="w-full bg-[#f7f7f7]">
+        <nav className="flex bg-[#0cc779] justify-between items-center px-3">
+          <p>aku</p>
+          <a onClick={(e) => handleOnClickLogout(e)}>Logout</a>
+        </nav>
+      </div>
 
       {/* <h1>Hello, {name}</h1>
       <Button onClick={(e) => handleOnClickLogout(e)}>logout</Button> */}
